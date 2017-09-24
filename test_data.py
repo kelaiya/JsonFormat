636093ea8f04f33dcdf65e45c93a6a25f2d321ce
Kelaiya <kelaiyarao1@gmail.com>
2017-09-24 12:49:26 -0400

    Update test comments

--- test_data.py
+++ test_data.py
@@ -1,9 +1,12 @@
 import unittest
 import firstPython
 
-ans1 = '{\n  "entries": [\n    {\n      "color": "yellow", \n      "firstName": "Quinton", \n      "lastName": "Liptak", \n      "phone": "(653)-889-7235", \n      "zipcode": "70703"\n    }, \n    {\n      "color": "aqua marine", \n      "firstName": "Ria", \n      "lastName": "Tillotson", \n      "phone": "196 910 5548", \n      "zipcode": "97671"\n    }\n  ], \n  "errors": [\n    2\n  ]\n}'
+# ans1 is the answer of dummy1.in file
+# ans2 is the answer of dummy2.in file
 
-ans2 = '{\n  "entries": [\n    {\n      "color": "blue", \n      "firstName": "Maurita", \n      "lastName": "Awong", \n      "phone": "061 937 1243", \n      "zipcode": "15726"\n    }, \n    {\n      "color": "red", \n      "firstName": "Englebert", \n      "lastName": "G.", \n      "phone": "839 014 8051", \n      "zipcode": "36410"\n    }\n  ], \n  "errors": [\n    0\n  ]\n}'
+ans1 = '{\n  "entries": [\n    {\n      "color": "yellow", \n      "firstname": "Quinton", \n      "lastname": "Liptak", \n      "phonenumber": "(653)-889-7235", \n      "zipcode": "70703"\n    }, \n    {\n      "color": "aqua marine", \n      "firstname": "Ria", \n      "lastname": "Tillotson", \n      "phonenumber": "196 910 5548", \n      "zipcode": "97671"\n    }\n  ], \n  "errors": [\n    2\n  ]\n}'
+
+ans2 = '{\n  "entries": [\n    {\n      "color": "blue", \n      "firstname": "Maurita", \n      "lastname": "Awong", \n      "phonenumber": "061 937 1243", \n      "zipcode": "15726"\n    }, \n    {\n      "color": "red", \n      "firstname": "Englebert", \n      "lastname": "G.", \n      "phonenumber": "839 014 8051", \n      "zipcode": "36410"\n    }\n  ], \n  "errors": [\n    0\n  ]\n}'
 
 class TestData(unittest.TestCase):
 

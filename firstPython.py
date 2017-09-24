21638cf93f5d23ba09e60d692a35faf3d3150933
Kelaiya <kelaiyarao1@gmail.com>
2017-09-24 12:43:18 -0400

    Phonenumber option

--- firstPython.py
+++ firstPython.py
@@ -22,12 +22,19 @@ def jsonOutput(data):
     if len(crr) < 4:
       mistake.append(i)
     elif crr[2].find('(') != -1 and len(crr[2]) == 14 and len(crr[4]) == 5:
-      entry.append({'firstName': crr[1], 'lastName': crr[0], 'color': crr[3], 'phone': crr[2], 'zipcode': crr[4] })
+
+      # if we don't want brackets("()") and dashes("-") in the phonenumber than execute the below command:
+      # crr[2] = crr[2].replace("(","").replace(")","").replace("-"," ")
+      
+      entry.append({'firstname': crr[1], 'lastname': crr[0], 'color': crr[3], 'phonenumber': crr[2], 'zipcode': crr[4] })
+    
     elif len(crr) == 4 and len(crr[2]) == 5 and len(crr[3]) == 12 :
       spl = crr[0].split(' ')
-      entry.append({'firstName': spl[0], 'lastName': spl[1], 'color': crr[1], 'phone': crr[3], 'zipcode': crr[2] })
+      entry.append({'firstname': spl[0], 'lastname': spl[1], 'color': crr[1], 'phonenumber': crr[3], 'zipcode': crr[2] })
+    
     elif ord(crr[2][0]) < 58 and ord(crr[2][0]) > 47 and len(crr[2]) == 5 and len(crr[3]) == 12 :
-      entry.append({'firstName': crr[0], 'lastName': crr[1], 'color': crr[4], 'phone': crr[3], 'zipcode': crr[2] })
+      entry.append({'firstname': crr[0], 'lastname': crr[1], 'color': crr[4], 'phonenumber': crr[3], 'zipcode': crr[2] })
+    
     else:
       mistake.append(i)
 
@@ -38,7 +45,7 @@ def jsonOutput(data):
   # stored_obj is an object stored by lastname
 
   sorted_obj = dict(obj) 
-  sorted_obj["entries"] = sorted(obj['entries'], key=lambda x: x['lastName'], reverse=False) 
+  sorted_obj["entries"] = sorted(obj['entries'], key=lambda x: x['lastname'], reverse=False) 
 
   # jsonAns is the Json format of that object
   
